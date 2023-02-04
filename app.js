90e1ed502443b50699960aae9fb8fde3f34f56a4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-04 15:46:47 -0600

    Helper Methods

---
+++ app.js
@@ -0,0 +1,7 @@
+// document.write("hello world");
+// alert("hello world");
+// console.log("hello world");
+
+document.write({ name: "praveen" });
+alert({ name: "praveen" });
+console.log({ name: "praveen" });
