994be67d48eaaf7e485cd62eeb5c953cf4a1a3b6
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-04 19:41:39 -0600

    Assign Variable Value Later

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Variable - Most Basic Building Block
 // Variables - Store, Access, Modify === Value
 // Declare, Assignment Operator, Assign Value
+// assign value later, modify existing
 
-let name = "Praveen Kumar";
-
-console.log(name);
-// some lines of code
-console.log(name);
-// some lines of code
-console.log(name);
-// some lines of code
-console.log(name);
-// some lines of code
+let name = "Praveen";
+// Declaration
+let address, zip, state;
+// Assignment
+address = "Macomb";
+zip = "61455";
+state = "IL";
+name = "Praveen Kumar";
+console.log(address, zip, state, name);
