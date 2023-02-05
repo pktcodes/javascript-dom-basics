d9604009f5dac55974a6ce25729cb4f4f71ae88e
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-04 20:11:22 -0600

    Variable Naming Rules

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-// Variable - Most Basic Building Block
-// Variables - Store, Access, Modify === Value
-// Declare, Assignment Operator, Assign Value
-// assign value later, modify existing
+// can contain digits, letters, underscore, $
+// must start with letter, $ or _
+let $random_123 = "random";
+console.log($random_123);
 
-let name = "Praveen";
-// Declaration
-let address, zip, state;
-// Assignment
-address = "Macomb";
-zip = "61455";
-state = "IL";
-name = "Praveen Kumar";
-console.log(address, zip, state, name);
+// no keyword
+// let let = '123'; ❌
+
+// cannot start with number
+// let 123$randomNumber = "random"; ❌
+
+// case sensitive - fullname vs Fullname
+let fullname = "praveen";
+let Fullname = "Praveen";
+console.log(fullname, Fullname);
+
+// camelCase or underscore - common convention
+let fullName = "praveenKumar";
+let full_name = "praveen_kumar";
+console.log(fullName);
+console.log(full_name);
