568082d74881d67f545737b0b6e4f15a775b4a1d
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-05 13:58:44 -0600

    Numbers Basics

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-// String Concatenation - combine string values
-// `` - backticks (template strings) easier option.
+// Numbers
+// Loosely Typed = we don't declare any type, by default javascript defines data type for variables depending on the input given. For strictly typed language like Java, we have Typescript
 
-const firstName = 'Praveen';
-const lastName = 'Kumar';
-let fullName = firstName + ' ' + lastName;
+const number = 32;
+let decimalNumber = 2.456;
+// const decimalString = '2.456';
+// decimalNumber = 'are good';
 
-console.log('Hello there, my fullname is : ' + fullName);
+const addition = number + decimalNumber;
+const subtraction = number - decimalNumber;
+const multiplication = number * decimalNumber;
+const division = number / decimalNumber;
 
-const website = 'google';
-const url = 'https://www.' + website + '.com';
-console.log(url);
+// console.log(number);
+// console.log(decimalNumber);
+// console.log(decimalString);
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
