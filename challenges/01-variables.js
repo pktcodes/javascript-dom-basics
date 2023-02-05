97120169b98f6fc204522fe0da99e43943c81df2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-04 21:58:28 -0600

    Variables Challenge

---
+++ challenges/01-variables.js
@@ -0,0 +1,21 @@
+/*
+## Variables #1
+
+1. create "firstName" and "last_name" variables
+2. assign your values
+3. create "address" variable and assign "main street"
+   value to it
+4. re-assign address to "first street" later
+5. log all values in the console 
+*/
+
+const firstName = "praveen";
+const last_name = "kumar";
+
+let address = "main street";
+address = "first street";
+
+// console.log(firstName);
+// console.log(last_name);
+// console.log(address);
+console.log(firstName, last_name, address);
