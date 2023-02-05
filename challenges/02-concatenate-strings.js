9c3c99b71e5096229e453c279c6af31d0604ebf3
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-04 23:23:34 -0600

    String Concatentation Challenge

---
+++ challenges/02-concatenate-strings.js
@@ -0,0 +1,15 @@
+/*
+## Concatenate Strings #2
+
+1. create "street" and "country" variables
+2. assign your values
+3. create "fullMailingAddress" variable and assign the result of "street + country"
+4. remember about the space
+5. log "fullMailingAddress" in the console
+*/
+
+const street = 'Wigwam Street';
+const country = 'USA';
+const fullMailingAddress = street + ', ' + country;
+
+console.log(fullMailingAddress);
