52923d0f105b230d41d4198a28f1a30d3548173a
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-05 21:41:12 -0600

    Functions - Declare, Invoke

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 // Arrays, Functions and Objects
-// Arrays - [], 0 index based
+// Functions - declare, invoke
 
-const friend1 = 'anna';
-const friend2 = 'susan';
-const friend3 = 'peter';
-const friend4 = 'john';
+function hello() {
+  console.log('There is Anna');
+  console.log('There is John');
+  console.log('There is Susan');
+}
 
-const friends = ['john', 'peter', 'anna', 'susan', 45, undefined, null];
-
-const bestfriend = friends[2];
-
-friends[4] = 'praveen';
-
-console.log(friends);
-console.log(friends[0]);
-console.log(friends[4]);
-console.log(bestfriend);
+hello();
+// some code here
+hello();
+// some code here
+hello();
