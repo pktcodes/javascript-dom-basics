dab80336c07ab984a71e466f9c6d2907160f6aa1
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-09 00:58:31 -0600

    Switch Statement

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
-// Logical Operators
-// (|| - OR), (&& - AND), !
+// Switch
+// dice values : 1 - 6
 
-const name = 'peter';
-const age = 24;
+const dice = 3;
 
-if (name !== 'peter' || age === 24) {
-  console.log('hello there, ' + name);
-} else {
-  console.log('wrong values');
+switch (dice) {
+  case 1:
+    console.log('you got one');
+    break;
+  case 2:
+    console.log('you got two');
+    break;
+  case 3:
+    console.log('you got three');
+    break;
+  case 4:
+    console.log('you got four');
+    break;
+  case 5:
+    console.log('you got five');
+    break;
+  case 6:
+    console.log('you got six');
+    break;
+  default:
+    console.log('you did not roll the dice');
 }
+
+/* Using if...else statement */
+// if (dice === 1) {
+//   console.log('you got one');
+// } else if (dice === 2) {
+//   console.log('you got two');
+// } else if (dice === 3) {
+//   console.log('you got three');
+// } else if (dice === 4) {
+//   console.log('you got four');
+// } else if (dice === 5) {
+//   console.log('you got five');
+// } else if (dice === 6) {
+//   console.log('you got six');
+// } else {
+//   console.log('you did not roll the dice');
+// }
+
+/* Using if statement */
+// if (dice === 1) {
+//   console.log('you got one');
+// }
+// if (dice === 2) {
+//   console.log('you got two');
+// }
+// if (dice === 3) {
+//   console.log('you got three');
+// }
+// if (dice === 4) {
+//   console.log('you got four');
+// }
+// if (dice === 5) {
+//   console.log('you got five');
+// }
+// if (dice === 6) {
+//   console.log('you got six');
+// }
+// if (dice < 1 || dice > 6) {
+//   console.log('you did not roll the dice');
+// }
