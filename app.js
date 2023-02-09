ccafd975be511a79414730944157441fbafa6aaa
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-09 15:18:08 -0600

    while loop

--- app.js
+++ app.js
@@ -1,67 +1,11 @@
-// Switch
-// dice values : 1 - 6
+// Loops
+// repeatedly run a block of code while condition is true
+// while loop
+// TURN OFF AUTOSAVE
 
-const dice = 3;
+let amount = 10;
 
-switch (dice) {
-  case 1:
-    console.log('you got one');
-    break;
-  case 2:
-    console.log('you got two');
-    break;
-  case 3:
-    console.log('you got three');
-    break;
-  case 4:
-    console.log('you got four');
-    break;
-  case 5:
-    console.log('you got five');
-    break;
-  case 6:
-    console.log('you got six');
-    break;
-  default:
-    console.log('you did not roll the dice');
+while (amount > 0) {
+  console.log('I have ' + amount + ' dollars and I am going to mall.');
+  amount--;
 }
-
-/* Using if...else statement */
-// if (dice === 1) {
-//   console.log('you got one');
-// } else if (dice === 2) {
-//   console.log('you got two');
-// } else if (dice === 3) {
-//   console.log('you got three');
-// } else if (dice === 4) {
-//   console.log('you got four');
-// } else if (dice === 5) {
-//   console.log('you got five');
-// } else if (dice === 6) {
-//   console.log('you got six');
-// } else {
-//   console.log('you did not roll the dice');
-// }
-
-/* Using if statement */
-// if (dice === 1) {
-//   console.log('you got one');
-// }
-// if (dice === 2) {
-//   console.log('you got two');
-// }
-// if (dice === 3) {
-//   console.log('you got three');
-// }
-// if (dice === 4) {
-//   console.log('you got four');
-// }
-// if (dice === 5) {
-//   console.log('you got five');
-// }
-// if (dice === 6) {
-//   console.log('you got six');
-// }
-// if (dice < 1 || dice > 6) {
-//   console.log('you did not roll the dice');
-// }
