ffbb48663fdedf87097d2089e193c207600ef2bc
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-08 23:28:28 -0600

    Equality

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 // Conditional Statements
 // Comparison Operators
 // >, <, >=, <=, ==, ===, !=, !==
-// else if and !
+// == checks only value
+// === checks value and type
 
 const num1 = 6;
 const num2 = 6;
-const result = num1 >= num2;
+// const num2 = '6'
 
-if (num1 > num2) {
-  console.log('num1 is greater than num2');
-} else if (result) {
-  console.log('num1 is equal to num2');
-} else {
-  console.log('num2 is greater than num1');
-}
+// const value1 = num1 == num2;
+// const value2 = num1 === num2;
 
-// !
-const value = false;
+const value1 = num1 != num2;
+const value2 = num1 !== num2;
 
-if (!value) {
-  console.log('value is not false');
-}
+console.log(value1);
+console.log(value2);
