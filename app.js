be5766a6cb98126d5c213fe150dfd279c0757510
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-09 23:22:29 -0600

    String Properties and Methods

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-// Loops
-// repeatedly run a block of code while condition is true
-// for loop
+// String properties and methods
+// wrapper String Object, don't memorize methods
 
-// let i;
+const text = ' Peter Jordon';
+const result = text.length;
+console.log(result);
 
-// for (i = 0; i < 10; i++) {
-//   console.log('I have ' + i + ' dollars');
-// }
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.charAt(0));
+console.log(text.charAt(text.length - 1));
+console.log(text.trim());
+console.log(text.indexOf('p'));
+console.log(text.indexOf('P'));
+console.log(text.trim().toLowerCase().startsWith('p'));
+console.log(text.slice(0, 2));
+console.log(text.slice(0, 6));
+console.log(text.slice(-3));
+console.log(text.includes('eter'));
 
-for (let number = 11; number >= 0; number--) {
-  console.log('I have ' + number + ' dollars');
-}
+/* Object properties and methods */
+// const person = {
+//   name: 'Peter Jordon', //property since it has value
+//   greeting() {
+//     console.log('Hello, This is Peter.');
+//   },
+// };
+
+// console.log(person.name);
+// person.greeting();
