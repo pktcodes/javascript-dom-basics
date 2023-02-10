533870f0c8a0d7d685925ce6e31dc75e0b60ec5e
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-10 00:03:55 -0600

    Template Literals

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-// String properties and methods
-// wrapper String Object, don't memorize methods
+// Template Literals - ES6+
+// Backtick characters `` - above tab (left from one)
+// Interpolation ${} - insert expression(value)
 
-const text = ' Peter Jordon';
-const result = text.length;
-console.log(result);
+const name = 'peter';
+const age = 25;
 
-console.log(text.length);
-console.log(text.toLowerCase());
-console.log(text.toUpperCase());
-console.log(text.charAt(0));
-console.log(text.charAt(text.length - 1));
-console.log(text.trim());
-console.log(text.indexOf('p'));
-console.log(text.indexOf('P'));
-console.log(text.trim().toLowerCase().startsWith('p'));
-console.log(text.slice(0, 2));
-console.log(text.slice(0, 6));
-console.log(text.slice(-3));
-console.log(text.includes('eter'));
+const sentence = "Hey it's " + name + ' and he is ' + age + ' years old';
+console.log(sentence);
 
-/* Object properties and methods */
-// const person = {
-//   name: 'Peter Jordon', //property since it has value
-//   greeting() {
-//     console.log('Hello, This is Peter.');
-//   },
-// };
-
-// console.log(person.name);
-// person.greeting();
+/* Using Template Literal */
+const value = `Hey it's ${name} and he is ${age} years old and here is the simple math: ${
+  5 + 6
+}`;
+console.log(value);
