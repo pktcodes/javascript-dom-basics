f60540ba90fa8ace22ba733e40029eca30786759
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-10 22:17:30 -0600

    Exercise - Full Name

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-// Array Properties and Methods
-const names = ['john', 'bobo', 'barry', 'olga', 'ben'];
+// Arrays and for loop
 
-// length
-console.log(names.length);
-console.log(names[names.length - 1]);
+const names = ['anna', 'susy', 'bob'];
+const lastName = 'shakeandbake';
+const newArray = [];
 
-// concat
-const otherNames = ['pepper', 'onion', 'banana'];
-const allNames = names.concat(otherNames);
-console.log(allNames);
+// for loop
+for (i = 0; i < names.length; i++) {
+  const fullName = `${names[i]} ${lastName}`;
+  newArray.push(fullName);
+}
 
-// reverse
-console.log(allNames.reverse());
-
-// unshift - insert item at start of array
-allNames.unshift('susan');
-console.log(allNames);
-
-// shift - remove item from start of array
-allNames.shift();
-console.log(allNames);
-
-// push - insert item at the end of array
-allNames.push('anna');
-console.log(allNames);
-
-// pop - remove item at the end of array
-allNames.pop();
-console.log(allNames);
-
-// splice - mutates the original array
-const specificNames = allNames.splice(1, 3);
-console.log(specificNames);
-console.log(allNames);
+console.log(names);
+console.log(newArray);
