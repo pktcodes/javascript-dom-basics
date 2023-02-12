bcb6563bf5817e496ce9e9fd950719e6ac6a84d5
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-11 23:55:45 -0600

    Local Scope

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-// Global Scope vs Local Scope
-// any variable outside code block {} is said to be in Global Scope
-// can be accesss anywhere in the program
-// Gotchas : name collisions, modify by mistake
+// Local Scope
+// no name collision, can not be access from outside code blocks
+// if - NOT VAR
 
 let name = 'anna';
-name = 'peter';
 
 function calculate() {
-  console.log(name);
-  name = 'orange';
-  function innerFunction(params) {
-    console.log(`Hi, my name is ${name} and I'm inner function`);
-    name = 'inner function';
-  }
-  innerFunction();
+  let name = 'anna';
+  const age = 25;
+  becomesGlobal = 'global variable';
+  //when keyword not used, javascript takes it as global variable
 }
 
 calculate();
+console.log(becomesGlobal);
+
+// Just code block
+{
+  name: 'bobo';
+  age: 25;
+}
 
 if (true) {
   // some other code.........
