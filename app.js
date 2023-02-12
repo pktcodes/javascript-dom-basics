54345b9a405cdd1e07e445028bf638482f8f824c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-11 19:39:16 -0600

    Exercise - Calculate Total

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-// Arrays and for loop
+// Functions, return, if, arrays, for loop
 
-const names = ['anna', 'susy', 'bob'];
-const lastName = 'shakeandbake';
-const newArray = [];
+const gas = [20, 30, 50, 40];
+const food = [30, 10, 20, 30];
 
-// for loop
-for (i = 0; i < names.length; i++) {
-  const fullName = `${names[i]} ${lastName}`;
-  newArray.push(fullName);
+function calculateTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  if (total > 100) {
+    console.log('Whoa! You are spending way too much.');
+    return total;
+  }
+  console.log('You are spending is less than 100.');
+  return total;
 }
 
-console.log(names);
-console.log(newArray);
+const gasTotal = calculateTotal(gas);
+const foodTotal = calculateTotal(food);
+const utilitiesTotal = calculateTotal([525 + 21 + 75 + 50]);
+
+console.log({
+  gas: gasTotal,
+  food: foodTotal,
+  utilities: utilitiesTotal,
+});
