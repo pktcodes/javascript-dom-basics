7c4154ec3b0aafc84185c4f295a7710473c6b6cd
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-12 16:32:12 -0600

    Array Iterators

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-// Callback Functions, Higher Order Functions, Functions as First Class Objects/Citizens
-// Functions are first class objects - stored in a variable (expression), passed as an argument to another function, return from the function (closure)
+// Powerful Array Methods
+// forEach, map, filter, find, reduce
+// Iterate over array - no for loop required
+// Accept CALLBACK function as an argument, calls Callback against each item in a array. Reference Item in the Callback Paramater.
 
-// Higher Order function - accepts another function as an argument or returns another function as a result
+const numbers = [0, 1, 2, 3, 4];
 
-// Callback Function - is afunction which is passed to a another function as an an argument and executed inside that function
-
-// Callback function
-function morning(name) {
-  return `Good morning ${name.toUpperCase()}`;
+/* for loop */
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
 }
-
-function afternoon(name) {
-  return `Good afternoon ${name.repeat(3)} `;
-}
-
-// Higher Order Funtion
-function greet(name, cb) {
-  const myName = 'praveen';
-  console.log(`${cb(name)}, my name is ${myName}.`);
-}
-
-greet('bobo', morning);
-greet('peter', afternoon);
-
-// function greetMorning(name) {
-//   const myName = 'praveen';
-//   console.log(`Good morning ${name}, my name is ${myName}`);
-// }
-// function greetAfternoon(name) {
-//   const myName = 'praveen';
-//   console.log(`Good afternoon ${name}, my name is ${myName}`);
-// }
-
-// greetMorning('bob');
-// greetAfternoon('peter');
