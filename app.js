3da57dc04cfd68fe372f19881e8f7f66fd6040d4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-12 16:13:53 -0600

    Callback Functions, Higher Order Functions

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-// Variable Lookup
-// {} - code block
+// Callback Functions, Higher Order Functions, Functions as First Class Objects/Citizens
+// Functions are first class objects - stored in a variable (expression), passed as an argument to another function, return from the function (closure)
 
-const globalNumber = 5;
+// Higher Order function - accepts another function as an argument or returns another function as a result
 
-function add(num1, num2) {
-  const globalNumber = 20;
-  const result = num1 + num2 + globalNumber;
+// Callback Function - is afunction which is passed to a another function as an an argument and executed inside that function
 
-  function multiply() {
-    const globalNumber = 10;
-    const multiplyResult = result * globalNumber;
-    console.log(multiplyResult);
-  }
-  // console.log(multiplyResult); - Accessing local to global will not work
-
-  multiply();
-  return result;
+// Callback function
+function morning(name) {
+  return `Good morning ${name.toUpperCase()}`;
 }
 
-console.log(add(3, 4));
+function afternoon(name) {
+  return `Good afternoon ${name.repeat(3)} `;
+}
+
+// Higher Order Funtion
+function greet(name, cb) {
+  const myName = 'praveen';
+  console.log(`${cb(name)}, my name is ${myName}.`);
+}
+
+greet('bobo', morning);
+greet('peter', afternoon);
+
+// function greetMorning(name) {
+//   const myName = 'praveen';
+//   console.log(`Good morning ${name}, my name is ${myName}`);
+// }
+// function greetAfternoon(name) {
+//   const myName = 'praveen';
+//   console.log(`Good afternoon ${name}, my name is ${myName}`);
+// }
+
+// greetMorning('bob');
+// greetAfternoon('peter');
