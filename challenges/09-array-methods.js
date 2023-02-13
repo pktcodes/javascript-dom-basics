1ec2341384fc932ed5385b2e39cee5c76b763c93
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-13 00:11:25 -0600

    Array Challenge - filter

--- challenges/09-array-methods.js
+++ challenges/09-array-methods.js
@@ -7,5 +7,16 @@ const updatedStudents = students.map(function (student) {
   student.role = 'student';
   return student;
 });
+// console.log(updatedStudents);
 
-console.log(updatedStudents);
+// filter
+const highScores = students.filter(function (student) {
+  // if (student.score >= 80) {
+  //   return student;
+  // }
+  // if (student.score >= 80) return student;
+
+  return student.score >= 80;
+});
+
+console.log(highScores);
