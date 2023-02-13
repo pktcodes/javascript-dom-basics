63f0e4f4a0954999ea65ffdc6602a3d6374e2c04
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-12 18:17:42 -0600

    map

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-// forEach
-// does not return new array
+// map
+// does return a new array
+// does not change size of original array
+// uses values from original array when making new one
 
 const people = [
   { name: 'bob', age: 25, role: 'developer' },
   { name: 'anna', age: 19, role: 'designer' },
   { name: 'susan', age: 23, role: 'boss' },
+  { name: 'jennifer', age: 18, role: 'programmer' },
 ];
 
-function showPeople(person) {
-  console.log(person.role);
-}
-
-// people.forEach(showPeople);
-
-people.forEach(function (person) {
-  console.log(person.role.toUpperCase());
+// returns same size - 4
+const newArray = people.map(function (person) {
+  return `hello world`;
 });
+
+console.log(newArray);
+
+const newPeople = people.map(function (person) {
+  return {
+    firstName: person.name.toUpperCase(),
+    oldAge: person.age + 20,
+  };
+});
+
+console.log(newPeople);
+
+const names = people.map(function (person) {
+  return `<h1>${person.name}</h1>`;
+});
+
+document.body.innerHTML = names.join('');
+console.log(names);
