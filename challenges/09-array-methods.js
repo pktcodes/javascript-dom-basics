ac3a7a968812e2e29eefc0ac363b28cd2a930e94
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-12 23:18:44 -0600

    Array Challenge - Split into Multiple Files

--- challenges/09-array-methods.js
+++ challenges/09-array-methods.js
@@ -1,34 +1,3 @@
-const students = [
-  {
-    id: 1,
-    name: 'peter',
-    score: 80,
-    favoriteSubject: 'math',
-  },
-  {
-    id: 2,
-    name: 'susan',
-    score: 99,
-    favoriteSubject: 'history',
-  },
-  {
-    id: 3,
-    name: 'anna',
-    score: 85,
-    favoriteSubject: 'math',
-  },
-  {
-    id: 4,
-    name: 'john',
-    score: 35,
-    favoriteSubject: 'arts',
-  },
-  {
-    id: 5,
-    name: 'bob',
-    score: 74,
-    favoriteSubject: 'math',
-  },
-];
-
 console.log(students);
+
+showName();
