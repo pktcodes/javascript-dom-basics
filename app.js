4f0187ff99e876b03d2261fb964fe3f7bcf31214
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-13 22:23:28 -0600

    Math Object

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-// reduce
-// iterates, callback function
-// reduces to a single value - number, array, object
-// 1 parameter ('acc') - total of all calculations
-// 2 parameter ('curr') - current iteration/value
+// Math
+// Standard built-in objects - always available
 
-const people = [
-  { name: 'bob', age: 25, role: 'developer', id: 1, salary: 200 },
-  { name: 'anna', age: 19, role: 'designer', id: 2, salary: 300 },
-  { name: 'susan', age: 23, role: 'boss', id: 3, salary: 500 },
-  { name: 'jennifer', age: 18, role: 'programmer', id: 4, salary: 200 },
-];
+/* floor - round down  */
+// const number = 4.56789;
+// const result = Math.floor(number);
 
-const total = people.reduce(function (accumulator, currentItem) {
-  console.log(`accumulator : ${accumulator}`);
-  console.log(`currentItem : ${currentItem.salary} `);
-  accumulator += currentItem.salary;
-  return accumulator;
-}, 0);
+/* ceil - round up  */
+// const number = 4.56789;
+// const result = Math.ceil(number);
 
-console.log(total);
+/* Finding squareroot */
+// const result = Math.sqrt(25);
+
+/* Finding minimum */
+// const result = Math.min(4, 1, 5, 3, 7, 2, 1000);
+
+/* Finding maximum */
+// const result = Math.max(4, 1, 5, 3, 7, 2, 1000);
+
+/* value of PI */
+// const result = Math.PI;
+
+/* random - to get numbers from 1 to 10*/
+// const result = Math.floor(Math.random() * 10) + 1;
+const result = Math.ceil(Math.random() * 10);
+
+console.log(result);
