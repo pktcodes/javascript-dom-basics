c913cdc77c5d8ce8e7be3184cced3966a72840fd
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-16 00:07:14 -0600

    Navigate the DOM - parentElement

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-// Select the element or group of elements the we want
-// Decide the effect we want to apply to the selection
+// parent
 
-// childNodes - returns all childNodes including whitespace which is treated as a text node
+const heading = document.querySelector('.secondHeading');
+// console.log(heading);
+const parent = heading.parentElement;
+parent.style.color = 'red';
 
-// children
-// firstChild
-// lastChild
-
-const fruits = document.querySelector('#fruits');
-// console.log(fruits);
-
-// returns the child nodes along with whitespace - like non-element nodes like text and comment
-console.log(fruits.childNodes);
-
-// returns only element nodes
-console.log(fruits.children);
-
-// returns along with whitespace
-console.log(fruits.firstChild);
-
-// returns along with whitespace
-console.log(fruits.lastChild);
+parent.parentElement.style.color = 'blue';
+console.log(parent.parentElement.parentElement.parentElement);
