c4fafdc4c3b9128f1d1b44737668b24563a47131
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-16 00:42:31 -0600

    Navigate the DOM - nextElementSibling, previousElementSibling

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-// previousSibling
-// nextSibling
-// return whitespace
+// previousElementSibling
+// nextElementSibling
+
+// does not return whitespace
 
 const firstItem = document.querySelector('.first');
-const secondSibling = firstItem.nextSibling;
-
-//return whitespace
-console.log(secondSibling);
-// returns the element
-console.log(secondSibling.nextSibling);
+firstItem.nextElementSibling.style.color = 'red';
 
 const lastItem = document.querySelector('#last');
-const thirdItem = lastItem.previousSibling.previousSibling;
-thirdItem.style.color = 'red';
-
-// return null when sibling is present
-console.log(lastItem.nextSibling.nextSibling);
+lastItem.previousElementSibling.style.color = 'blue';
