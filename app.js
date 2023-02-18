2e7731c554401060136b3cbfa2ba85d2e86d211c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-18 16:27:14 -0600

    Function Reference

--- app.js
+++ app.js
@@ -5,7 +5,17 @@
 const heading = document.querySelector('h1');
 
 const btn = document.querySelector('.btn');
-btn.addEventListener('click', function () {
+
+function changeHeadingColor() {
+  const hasClass = heading.classList.contains('red');
+
+  if (hasClass) {
+    heading.classList.remove('red');
+  } else {
+    heading.classList.add('red');
+  }
+
   console.log('Hey, You Clicked Me.');
-  heading.classList.add('red');
-});
+}
+
+btn.addEventListener('click', changeHeadingColor);
