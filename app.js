d46b70e72161a70d6f196a75718618b385934894
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-17 20:52:50 -0600

    prepend(), innerText

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-// createElement('element')
-// createTextNode('text content') - between tags
-// element.appendChild(childElement) - connect child to parent
+// prepend() - it adds at the start unlike append() which used to add at the end after js script tag
+// innerText - a better approach for adding text instead of create text node and appending to element
 
-// replaceChild('new','old'); - replace old element with new element
+const heading = document.createElement('h2');
 
-const result = document.getElementById('result');
-const heading2 = document.querySelector('.red');
+// add text to heading
+heading.innerText = 'i am dynamic heading';
+console.log(heading);
 
-// for div
-// create Element
-const divInBody = document.createElement('div');
-// Create text inside element
-const divInBodyText = document.createTextNode('i am simple div  in body');
-// attaching text as child to element
-divInBody.appendChild(divInBodyText);
-// attaching div element as child to body
-document.body.insertBefore(divInBody, result);
-
-// heading
-const headingInResultDiv = document.createElement('h2');
-const headingInResultDivText = document.createTextNode('dynamic heading');
-headingInResultDiv.appendChild(headingInResultDivText);
-headingInResultDiv.classList.add('blue');
-result.insertBefore(headingInResultDiv, heading2);
-
-const newHeading = document.createElement('h6');
-const newHeadingText = document.createTextNode(
-  'This is new heading h6 replacing div in body'
-);
-newHeading.appendChild(newHeadingText);
-newHeading.classList.add('red');
-document.body.replaceChild(newHeading, divInBody);
-
-console.log(result.children);
+// add element before
+document.body.prepend(heading);
