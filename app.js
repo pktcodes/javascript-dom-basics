a50affd2002afa8d08d8e3ebfc92ca330accfc8d
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-19 00:03:57 -0600

    Forms

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-// event propagation - order the events are fired/registered.
-// event bubbling - clicked element first then bubbles up -- default
-// event capturing - fires at the root and fires until reaches target
+// submit event listener
+// prevent default
+// how to get a value
 
-const container = document.querySelector('.container');
-const btn = document.querySelector('.btn');
-// const heading = document.querySelector('.heading');
-// console.log(heading);
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const password = document.getElementById('password');
 
-function sayHello() {
-  console.log('Hey, You Clicked Me.');
-}
-
-btn.addEventListener('click', function () {
-  const dynamicHeading = document.createElement('h1');
-  dynamicHeading.classList.add('heading');
-  dynamicHeading.textContent = "i'm heading inside the container";
-  container.appendChild(dynamicHeading);
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  console.log('Hey, You Clicked Me');
+  console.log(name.value);
+  console.log(password.value);
 });
-
-container.addEventListener('click', function (event) {
-  if (event.target.classList.contains('heading')) {
-    console.log('hello there');
-  }
-});
-
-// heading.addEventListener('click', sayHello);
