5b7447d5af8e8926ba37cc73436b8aeeb1cde915
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-18 20:51:06 -0600

    CurrentTarget vs Target

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-// event object argument e,evt
-// info about triggered event
-// event.type
-// event.currentTarget
-// this keyword
-// preventDefault() - prevents default behaviour
+// currentTarget - always refers to the element to which the event handler has been attached to
+// target - identifies the element on which the event occured
 
-const heading = document.querySelector('h1');
-// console.log(heading);
-heading.addEventListener('click', function (event) {
-  console.log(event.currentTarget);
-  event.currentTarget.classList.add('red');
-  console.log(event.type);
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+buttons.forEach(function (btn) {
+  btn.addEventListener('click', function (event) {
+    // currentTarget
+    console.log('current Target:', event.currentTarget);
+    event.currentTarget.style.color = 'green';
+
+    // Target
+    console.log('Target:', event.target);
+    event.target.style.color = 'green';
+  });
 });
-
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', (e) => {
-  console.log(e);
-  e.currentTarget.classList.add('blue');
-  // this works similar but doesn't work good with arrow functions
-  console.log(this);
-});
-
-// preventing the default behavior happening i.e here going upto page
-function linkClick(event) {
-  event.preventDefault();
-}
-
-const link = document.getElementById('link');
-link.addEventListener('click', linkClick);
