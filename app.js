b95a75606af90deaee620a194b8c31d8ce09c8ea
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-19 17:13:02 -0600

    setInterval()

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 /* 
-setTimeout - runs function once after specific time
+setInterval - runs function repeatedly at specific intervals
 
 - pass function reference / anonymous function - traditional, arrow
 - duration in ms (1000 ms = 1 second)
 - default 0, will cover more extensively in async/await section
 - returns unique identifier
-- clearTimeout to cancel
+- clearInterval to cancel
 - on window object 
 */
 
-function sayHello() {
-  console.log('Hi, I am John');
-}
-
-// setTimeout(sayHello, 1000);
-
-// will not work as expected - why? the function gets immediately invoked when page loads
-// setTimeout(sayHello(), 1000);
-
-// alternative approach - ES6 arrow functions
-setTimeout(function () {
-  // console.log('Hello there, I am John');
-}, 1000);
-
-// pass arguments
 function showScore(name, score) {
-  console.log(`hello ${name}, your score is ${score}`);
+  console.log(`Hi ${name}, your score is ${score}`);
 }
-// setTimeout(showScore, 1000, 'peter', '35');
 
-// clearTimeout
-const firstID = setTimeout(showScore, 1000, 'peter', '35');
-const secondID = setTimeout(showScore, 1000, 'john', '40');
+// setInterval(showScore, 1000);
 
-clearTimeout(firstID);
+const firstID = setInterval(showScore, 1000, 'peter', 35);
+// console.log(firstID);
+const secondID = setInterval(showScore, 4000, 'susan', 50);
 
-// on window
-// window.setTimeout();
+// clearInterval
+clearInterval(firstID);
