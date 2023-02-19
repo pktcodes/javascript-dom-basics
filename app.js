5169596b6bf3e7ae9b3ac62aae31c69e2f874a1f
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-19 14:40:51 -0600

    Local Storage With Multiple Values

--- app.js
+++ app.js
@@ -2,19 +2,31 @@
 // session storage - persisting data between sessions i.e the current browser tab
 // local storage - persisting data when opening and closing browser
 // setItem, getItem , removeItem, clear
+// JSON.stringify(), JSON.parse
 
-// localStorage.setItem('name', 'praveen');
-// sessionStorage.setItem('name', 'kumar');
+const friends = ['praveen', 'john', 'peter'];
 
-localStorage.setItem('name', 'praveen');
-localStorage.setItem('friend', 'kumar');
-localStorage.setItem('job', 'web developer');
-localStorage.setItem('address', 'illinois');
+// localStorage.setItem('friends', friends);
 
-const name = localStorage.getItem('name');
-console.log(name);
+/* problem - when access complex data types like array gives back strings when accessing */
+// const values = localStorage.getItem('friends');
+// console.log(values[1]);
 
-const anotherName = localStorage.removeItem('name');
-console.log(anotherName);
+// solution
+localStorage.setItem('friends', JSON.stringify(friends));
 
-localStorage.clear();
+const values = JSON.parse(localStorage.getItem('friends'));
+console.log(values[0]);
+
+let fruits;
+if (localStorage.getItem('fruits')) {
+  fruits = JSON.parse(localStorage.getItem('fruits'));
+} else {
+  fruits = [];
+}
+
+console.log(fruits);
+// fruits.push('apple');
+fruits.push('orange');
+console.log(fruits);
+localStorage.setItem('fruits', JSON.stringify(fruits));
